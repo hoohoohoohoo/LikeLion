566ceb4c6bb11579163732ae5320e7e7068f0ca3
hoohoohoohoo <hojun0520@nate.com>
2019-05-10 17:27:17 +0900

    week5

---
+++ 2주차/python/Test.py
@@ -0,0 +1,62 @@
+print("hello python")
+a=1
+b=2
+print(a+b)
+
+a1 = 3.5
+b1 = 1.2
+print(a1+b1)
+
+str1 = "졸려"
+str2 = "자고싶다"
+str3 = "지금은 새벽 3시"
+print(str1 + str2 + str3)
+print(str1 * 5)
+
+mystring = "pythom zz"
+print(mystring[0])
+print(mystring[6]) 
+print(mystring[8])
+print(mystring[-1])
+
+mystring="abc"
+print(len(mystring))
+
+mystring ="hello world"
+print(mystring[0:5])
+print(mystring[6:11])
+print(mystring[6:])
+print(mystring[:5])
+print(mystring[6:-1])
+print(mystring[::-1])
+
+print(1+2) #더하기
+print(5-3) #빼기
+print(2*4) #곱하기
+print(4/2) #나누기
+print(6//4) #몫
+print(6%4) #나머지
+print(2**3) #제곱
+a= 1
+a += 1 #a=a+1과 같은 의미
+print(a)
+#문자열 입력
+a2 = input("입력해봐용: ")
+print(a2)
+print(type(a2))
+#정수 입력
+a3 = int(input("입력하시오: "))
+print(a3)
+print(type(a3))
+
+number = 12
+number_f = float(number)
+number_s = str(number)
+print(type(number))
+print(type(number_f))
+print(type(number_s))
+
+string = "abc"
+string = int(string)
+
+
